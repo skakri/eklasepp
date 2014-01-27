0fe04567fed8d2cc8d2d2417166047765007954b
Kristaps Karlsons <kristaps.karlsons@gmail.com>
2014-01-27 11:57:54 +0200

    Cleanup, minor improvements.

--- src/eklasepp.js
+++ src/eklasepp.js
@@ -1,38 +1,50 @@
-var paras = document.getElementsByTagName("p");
-for (i = 0; i < paras.length; ++i) {
-    if (parseInt(Math.random() * 3, 10) === 0) {
-        paras[i].textContent = 'Labtien! ' + paras[i].textContent;
+(function () {
+    'use strict';
+
+    var classes = ('tvnet apollo diena chaulatv adv_urls_t adv_urls_b ' +
+        'adv_urls left_banner right_banner').split(' ');
+
+    var ids = 'top_adv_box top_adv_box_r top_sub_nav'.split(' ');
+
+    var something = 'ICAgX18gICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgX1' +
+            '9fXyAgICAgICAgXyAgIF8gICAgICBfICAgXyAgICANCiAvJ18gYFwgICBfX19f' +
+            'X19fICAgX19fX19fXyAgIF9fX19fX18gL1wgIF9gXCAgICAvJyBcLycgXCAgIC' +
+            '8nIFwvJyBcICAgLycgXC8nIFwNCi9cIFxMXCBcIC9cX19fX19fXCAvXF9fX19f' +
+            'X1wgL1xfX19fX19cXCBcIFwvXCBcIC9cXy9cX18vLyAgL1xfL1xfXy8vICAvXF' +
+            '8vXF9fLy8NClwvXz4gXyA8X1wvX19fX19fL19cL19fX19fXy9fXC9fX19fX18v' +
+            'X1wgXCBcIFwgXFwvL1wvX18vICAgXC8vXC9fXy8gICBcLy9cL19fLyANCiAgL1' +
+            'wgXExcIFwgL1xfX19fX19cIC9cX19fX19fXCAvXF9fX19fX1xcIFwgXF9cIFwN' +
+            'CiAgXCBcX19fXy8gXC9fX19fX18vIFwvX19fX19fLyBcL19fX19fXy8gXCBcX1' +
+            '9fXy8NCiAgIFwvX19fLyAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAg' +
+            'IFwvX19fLyANCg==';
+
+    var removeByClass = function (className) {
+        var elements = document.getElementsByClassName(className);
+
+        for (var i = 0; i < elements.length; i++) {
+            var child = elements[i];
+            var parent = child.parentNode;
+            parent.removeChild(child);
+            parent.parentNode.removeChild(parent);
+        }
+    };
+
+    var removeById = function (id) {
+        var element = document.getElementById(id);
+
+        if (element) {
+            element.parentNode.removeChild(element);
+        }
+    };
+
+    for (var i = 0; i < classes.length; i++) {
+        removeByClass(classes[i]);
     }
-}
-
-var removeShit = function (className) {
-    var shits = document.getElementsByClassName(className);
-    for (i = 0; i < shits.length; ++i) {
-        var parent = shits[i].parentNode;
-        parent.removeChild(shits[i]);
-        parent.parentNode.removeChild(parent);
+    for (var e = 0; e < ids.length; e++) {
+        removeById(ids[e]);
     }
-}
 
-var removeOtherShit = function (id) {
-    var shit = document.getElementById(id)
-    if (shit) {
-        shit.parentNode.removeChild(shit);
+    if (parseInt(Math.random() * 9, 10) === 0) {
+        console.log(atob(something)); // Heh.
     }
-}
-
-removeShit('tvnet');
-removeShit('apollo');
-removeShit('diena');
-removeShit('chaulatv');
-removeShit('adv_urls_t');
-removeShit('adv_urls_b');
-removeShit('adv_urls');
-removeShit('right_banner');
-removeShit('left_banner');
-
-removeOtherShit('top_adv_box');
-removeOtherShit('top_adv_box_r');
-removeOtherShit('top_sub_nav');
-
-console.log('8===D~~~')
+})();
